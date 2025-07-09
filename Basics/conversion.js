092aebb2acf2232672de70b1d9d4a0cd06be39e5
ananyajha11 <ananyaajha1103@gmail.com>
2025-07-09 07:34:54 +0000

    conversion of dt

---
+++ Basics/conversion.js
@@ -0,0 +1,12 @@
+let score = 33
+console.log(typeof score)
+
+let sc = "33AA"
+let b = Number(sc)
+console.log(b)//NaN
+
+//if sc=null then value->0
+//undefined ->Nan
+//true/false -> 1,0
+
+//typeof NaN -> number
